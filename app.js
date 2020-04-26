d5f47632c5b2912e7fb1bd00cf67d467c2ff076c
sujan <sujanpoojary884@gmail.com>
2020-04-26 17:49:30 +0530

    mail sent using nexmo

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
+const nexmo = require('nexmo');
+
+//init app
+const app = express();
+
+// init Nexmo
+const nexmoOb = new nexmo(
+  {
+    apiKey: '7b777576',
+    apiSecret: 'RhUhSBrVSctLmw0e',
+  },
+  { debug: true }
+);
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+  const number = req.body.number;
+  const text = req.body.text;
+  nexmoOb.message.sendSms(
+    '917022406442',
+    number,
+    text,
+    {
+      type: 'unicode',
+    },
+    (err, resdata) => {
+      if (err) {
+        console.log('err');
+      } else {
+        console.dir(resdata);
+      }
+    }
+  );
+});
+
+// server setup
+const port = 5000;
+const server = app.listen(port, () => {
+  console.log(`server running in port ${port}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+const numberInput = document.getElementById('number'),
+  textInput = document.getElementById('msg'),
+  button = document.getElementById('button'),
+  response = document.querySelector('.response');
+
+function send() {
+  const number = numberInput.value;
+  const text = textInput.value;
+  console.log('mainjs');
+  fetch('/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ number: number, text: text }),
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+button.addEventListener('click', send, false);
